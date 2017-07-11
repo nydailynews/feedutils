e2f0f1da6d784cd7e2cf629e2a330f8b80cb5d32
freejoe76 <jmurphy@denverpost.com>
2017-07-11 13:52:36 -0400

    Start on the feed output handling

---
+++ printjob.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Print the results of a recentfeed/json ingestion.
+import doctest
+import json
+import argparse
+
+class Format():
+    """ Manage the way we're outputting this stuff.
+        """
+
+    def __init__():
+        """
+            """
+        pass
+
+class JobConfig():
+    """ Streamline creating new print jobs.
+        """
+
+    def __init__():
+        """
+            """
+        pass
+
